548d51cf6fda8661985709e6a5a910112483d677
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-18 16:18:56 -0600

    Makes all but one test pass

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -4,16 +4,17 @@ describe "User creates a new job" do
   scenario "a user can create a new job" do
     company = Company.create(name: "ESPN")
     visit "/companies/#{company.id}/jobs"
-    save_and_launch_page
-    click_button "New Job"
+		
+    click_link "New Job"
 
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
+		select "Category 1", :from => "job[category_id]"
 
-    click_button "Create"
-
+    save_and_open_page
+    click_button "Create_Job"
     expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -4,25 +4,29 @@ describe Job do
   describe "validations" do
     context "invalid attributes" do
       it "is invalid without a title" do
-        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+				category = Category.new(title: "Sports")
+        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver", category: category)
         expect(job).to be_invalid
       end
 
       it "is invalid without a level of interest" do
-        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver")
+        category = Category.new(title: "Sports")
+        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver", category: category)
         expect(job).to be_invalid
       end
 
       it "is invalid without a city" do
-        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80)
+        category = Category.new(title: "Sports")
+        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80, category: category)
         expect(job).to be_invalid
       end
     end
 
     context "valid attributes" do
-      it "is valid with a title, level of interest, and company" do
+      it "is valid with a title, level of interest, a company, and a category" do
+        category = Category.new(title: "Sports")
         company = Company.new(name: "Turing")
-        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company)
+        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company, category: category)
         expect(job).to be_valid
       end
     end
@@ -30,7 +34,8 @@ describe Job do
 
   describe "relationships" do
     it "belongs to a company" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+      category = Category.new(title: "Sports")
+      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo", category: category)
       expect(job).to respond_to(:company)
     end
   end
